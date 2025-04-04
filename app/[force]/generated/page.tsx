13e8ed601e05f3d2afbe5e0fb70b219d2918461b
flow <flow@hrmm.xyz>
2025-04-04 15:58:12 +0900

    generateStaticParams

---
+++ app/[force]/generated/page.tsx
@@ -0,0 +1,16 @@
+export function generateStaticParams() {
+  return [{ force: 'force' }];
+}
+export const revalidate = 60;
+
+interface Props {
+  params: Promise<{ force: string }>;
+}
+
+export default async function Page({ params }: Props) {
+  const { force } = await params;
+  return <section>
+    <p>Page rendered: {new Date().toLocaleString()}</p>
+    <p>force: {force}</p>
+  </section>
+}
