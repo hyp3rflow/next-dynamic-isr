ce13e7acabb7d19305cd6040298d5e61d5890e05
flow <flow@hrmm.xyz>
2025-04-04 15:51:40 +0900

    dynamic/static

---
+++ app/[force]/static/page.tsx
@@ -0,0 +1,14 @@
+export const dynamic = 'force-static';
+export const revalidate = 60;
+
+interface Props {
+  params: Promise<{ force: string }>;
+}
+
+export default async function Page({ params }: Props) {
+  const { force } = await params;
+  return <section>
+    <p>Page rendered: {new Date().toLocaleString()}</p>
+    <p>force: {force}</p>
+  </section>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,21 @@
-export default function RootLayout({
+import { headers } from "next/headers";
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const header = await headers();
+  const length = [...header.entries()].length;
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <p>rendered time: {new Date().toLocaleString()}</p>
+        <p>header-length: {length}</p>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
